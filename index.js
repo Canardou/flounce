ab2fa6f8c1e79950e45016c3c97bb03d5f405bb7
canardou <hit_@hotmail.fr>
2014-09-10 19:29:14 +0000

    Starting files Hello world

---
+++ index.js
@@ -0,0 +1,13 @@
+var http=require('http');
+var path=require('path');
+
+var express=require('express');
+
+var router=express();
+var server=http.createServer(router);
+
+router.use(express.static(path.resolve(__dirname,'public')));
+
+server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
+console.log("Server listening at ", process.env.IP + ":" + process.env.PORT);
+});
