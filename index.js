43d9cb43052457a5a740f5aae7b1e8652c71da15
SimonR <simon.robain@gmail.com>
2014-09-11 15:04:05 +0200

    minor change: port 3000

--- index.js
+++ index.js
@@ -9,5 +9,5 @@ var server=http.createServer(router);
 router.use(express.static(path.resolve(__dirname,'public')));
 
 server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
-console.log("Server listening at ", process.env.IP + ":" + process.env.PORT);
+console.log("Server listening at port "+ (process.env.PORT || 3000));
 });
