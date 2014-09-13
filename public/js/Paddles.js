eb748028d80127016e61fb6824d1d694a2f31146
canardou <hit_@hotmail.fr>
2014-09-13 13:50:44 +0000

    Utils and Paddles.js
    Added global utils. Added destroy method to paddles.

--- public/js/Paddles.js
+++ public/js/Paddles.js
@@ -1,8 +1,9 @@
+/*global pi,cos,sin,def,isDef,rand from utils.js*/
 var Paddles=function(game,x,y,orientation,small,initial_angle,angle_max,speed){
     //Constants and functions
-    this.initial_angle = initial_angle||0.5;
-    this.angle_max = angle_max||0.9;
-    this.speed = speed||10;
+    this.initial_angle = def(initial_angle,0.5);
+    this.angle_max = def(angle_max,0.9);
+    this.speed = def(speed,10);
     var generate_paddle=function(cx,cy,img){
         var paddle=game.add.sprite(x,y,img);
         paddle.pivotOffsetX = cx;
@@ -10,7 +11,7 @@ var Paddles=function(game,x,y,orientation,small,initial_angle,angle_max,speed){
         game.physics.p2.enableBody(paddle,true);
         paddle.body.clearShapes();
         return paddle;
-    }
+    };
     
     this.small=small||false;
     switch(orientation){
@@ -44,11 +45,14 @@ var Paddles=function(game,x,y,orientation,small,initial_angle,angle_max,speed){
     this.paddle.pivotPoint=game.add.sprite(this.paddle.position.x + this.paddle.pivotOffsetX,
                                             this.paddle.position.y + this.paddle.pivotOffsetY);
     
-    
     game.physics.p2.enable(this.paddle.pivotPoint,true);
     this.paddle.pivotPoint.body.static=true;
     this.paddle.pivotPoint.body.clearCollision(true,true);
-    this.paddle.flipperConstraint=game.physics.p2.createRevoluteConstraint(this.paddle, [this.paddle.pivotOffsetX,  this.paddle.pivotOffsetY], this.paddle.pivotPoint, [0, 5]);
+    this.paddle.flipperConstraint=game.physics.p2.createRevoluteConstraint(this.paddle, 
+                                                                            [this.paddle.pivotOffsetX,this.paddle.pivotOffsetY],
+                                                                            this.paddle.pivotPoint,
+                                                                            [0, 5]);
+    
     this.paddle.flipperConstraint.upperLimitEnabled=true;
     this.paddle.flipperConstraint.lowerLimitEnabled=true;
     this.paddle.flipperConstraint.lowerLimit=this.orientation*this.initial_angle;
@@ -56,18 +60,23 @@ var Paddles=function(game,x,y,orientation,small,initial_angle,angle_max,speed){
     
     this.paddle.flipperConstraint.enableMotor();
     this.paddle.flipperConstraint.setMotorSpeed(this.orientation*this.speed);
-}
+};
 
 Paddles.prototype.up=function(x,y){
     if(this.orientation==1)
         this.paddle.flipperConstraint.lowerLimit=this.orientation*this.initial_angle-this.angle_max;
     else
         this.paddle.flipperConstraint.upperLimit=this.orientation*this.initial_angle+this.angle_max;
-}
+};
 
 Paddles.prototype.down=function(x,y){
     if(this.orientation==1)
         this.paddle.flipperConstraint.lowerLimit=this.orientation*this.initial_angle;
     else
         this.paddle.flipperConstraint.upperLimit=this.orientation*this.initial_angle;
-}
+};
+
+Paddles.prototype.destroy=function(){
+    this.paddle.pivotPoint.destroy();
+    this.paddle.destroy();
+};
---
+++ public/js/utils.js
@@ -0,0 +1,20 @@
+var pi=Math.PI;
+var cos=Math.cos;
+var sin=Math.sin;
+var inh=function(dst, src, crush){
+	for(var e in src.prototype){
+		if(!(e in dst.prototype)||crush)
+			dst.prototype[e]=src.prototype[e];
+	}
+};
+var isDef=function(arg){
+	return (typeof arg!=='undefined')?true:false;
+};
+var def=function(arg,def){
+	return isDef(arg)?def:arg;
+};
+var rand=function(max,min){
+	max=def(max,1);
+	min=def(min,0);
+	return Math.random()*(max-min)+min;
+};
