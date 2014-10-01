c892b18b2611891285f989b248ea5c8ec9b50118
canardou <hit_@hotmail.fr>
2014-10-01 11:49:02 +0000

    New background for test

--- public/js/load.js
+++ public/js/load.js
@@ -21,7 +21,7 @@ var loadState = {
 		game.load.image('guy_head', 'asset/sprites/guy_head.png');
 		game.load.image('paddle_right', 'asset/sprites/paddle_right.png');
 		game.load.image('paddle_left', 'asset/sprites/paddle_left.png');
-		game.load.image('background', 'asset/sprites/design.jpg');
+		game.load.image('background', 'asset/sprites/patron.png');
 		game.load.image('hint', 'asset/sprites/hint.png');
 		game.load.image('wood_frame', 'asset/sprites/wood_panel.png');
 		game.load.spritesheet('button', 'asset/sprites/GreenButton.png');
