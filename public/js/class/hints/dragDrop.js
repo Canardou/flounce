b2ed6dc5dbb0cae94dc0c76761ca88ace273605e
canardou <hit_@hotmail.fr>
2014-10-10 15:53:19 +0000

    Hand tutorial

---
+++ public/js/class/hints/dragDrop.js
@@ -0,0 +1,23 @@
+var dragExample = function() {
+    var hand = game.add.sprite(game.global.width / 2, game.global.height * 2 / 3, 'hand');
+    hand.alpha = 0;
+    hand.tween = game.add.tween(hand).to({
+        x: 50,
+        y: 990,
+        alpha: 1
+    }, 3000, Phaser.Easing.Exponential.Out, true, 0, false).onComplete.add(function() {
+        var tower = game.add.sprite(hand.x + 20, hand.y + 10, 'bumper0', 0);
+        tower.anchor.set(0.5);
+        tower.tween = game.add.tween(tower).to({
+            x: 450,
+            y: 450,
+            alpha:0
+        }, 4000, Phaser.Easing.Sinusoidal.InOut, true).onComplete.add(function() {tower.destroy()});
+        hand.bringToTop();
+        hand.tween = game.add.tween(hand).to({
+            x: 430,
+            y: 440,
+            alpha:0
+        }, 4000, Phaser.Easing.Sinusoidal.InOut, true).onComplete.add(function() {hand.destroy()});
+    }, hand);
+}
